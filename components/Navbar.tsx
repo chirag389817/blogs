520f89d272994ab13dc0216a0dc75b01713951f9
chirag389817 <chirag389817@gmail.com>
2024-06-09 10:13:52 +0530

    added:Navbar && fix:ThemeSwitch

---
+++ components/Navbar.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useRef } from "react";
+import ThemeSwitch from "./ThemeSwitch";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+
+function NavLink({
+    href,
+    children
+}: {
+    href: string;
+    children: React.ReactNode;
+}) {
+    return (
+        <a
+            className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+            href={href}
+        >
+            {children}
+        </a>
+    );
+}
+
+export default function Navbar() {
+    const menuRef = useRef<HTMLElement>(null);
+    const openBtnRef = useRef<HTMLButtonElement>(null);
+    const closeBtnRef = useRef<HTMLButtonElement>(null);
+    const openMenu = (e: React.MouseEvent) => {
+        (menuRef.current as HTMLElement).style.display = "flex";
+        (openBtnRef.current as HTMLButtonElement).style.display = "none";
+        (closeBtnRef.current as HTMLButtonElement).style.display = "block";
+    };
+    const closeMenu = (e: React.MouseEvent) => {
+        (menuRef.current as HTMLElement).style.display = "none";
+        (openBtnRef.current as HTMLButtonElement).style.display = "block";
+        (closeBtnRef.current as HTMLButtonElement).style.display = "none";
+    };
+    return (
+        <div className="antialiased bg-gray-100 dark:bg-gray-900">
+            <div className="w-full text-gray-700 bg-white dark:text-gray-200 dark:bg-gray-800">
+                <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+                    <div className="flex flex-row items-center justify-between p-4">
+                        <a
+                            href="/"
+                            className="flex items-center gap-4 text-lg font-bold tracking-widest text-gray-900 rounded-lg dark:text-white focus:outline-none focus:shadow-outline"
+                        >
+                            <Image
+                                className="h-8 w-8 mx-auto object-contain"
+                                width={2048}
+                                height={2048}
+                                src="/favicon.ico"
+                                alt="logo"
+                            />
+                            CSP Blogs
+                        </a>
+                        <div className="md:hidden flex gap-4 items-center">
+                            <ThemeSwitch />
+                            <button
+                                className="rounded-lg focus:outline-none focus:shadow-outline"
+                                ref={openBtnRef}
+                                onClick={openMenu}
+                            >
+                                <svg
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </button>
+                            <button
+                                className="hidden rounded-lg md:hidden focus:outline-none focus:shadow-outline"
+                                ref={closeBtnRef}
+                                onClick={closeMenu}
+                            >
+                                <svg
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <nav
+                        className="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:items-center md:flex-row"
+                        ref={menuRef}
+                    >
+                        <NavLink href="/categories">Categories</NavLink>
+                        <NavLink href="#">Portfolio</NavLink>
+                        <NavLink href="/#about">About</NavLink>
+                        <ThemeSwitch className="hidden md:block md:ml-4" />
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+}
--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function ThemeSwitch() {
+export default function ThemeSwitch({ className }: { className?: string }) {
     const [isDarnkmode, setIsDarkmode] = useState(false);
     useEffect(() => {
         if (
@@ -23,38 +24,35 @@ export default function ThemeSwitch() {
         setIsDarkmode(!isDarnkmode);
     };
     return (
-        <div>
-            <button
-                className="w-20 h-10 rounded-full bg-white flex items-center transition duration-300 focus:outline-none shadow"
-                onClick={toggleTheme}
+        <button
+            className={twMerge(
+                "text-yellow-500 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 w-9 h-9 relative rounded-full transition duration-300 p-2",
+                className
+            )}
+            onClick={toggleTheme}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
             >
-                <div
-                    className={`translate-x-[var(--theme-switch-translate-x)] bg-[var(--theme-switch-color)] w-12 h-12 relative rounded-full transition duration-300 p-1 text-white`}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        {isDarnkmode ? (
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                            />
-                        ) : (
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                            />
-                        )}
-                    </svg>
-                </div>
-            </button>
-        </div>
+                {!isDarnkmode ? (
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                ) : (
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                )}
+            </svg>
+        </button>
     );
 }
